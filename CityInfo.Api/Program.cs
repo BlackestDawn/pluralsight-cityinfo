0a76632053de9694cde01eacdb97519db4e80ada
Alexander Stauch <blacke4dawn@gmail.com>
2026-06-08 09:09:37 +0200

    aded Serilog as logging handler

--- CityInfo.Api/Program.cs
+++ CityInfo.Api/Program.cs
@@ -8,8 +8,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Scalar.AspNetCore;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+  .MinimumLevel.Debug()
+  .WriteTo.Console()
+  .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+  .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
